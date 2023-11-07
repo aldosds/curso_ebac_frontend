7f4a1d3253de3033aa47ed3166b40a9bd64c16db
Aldo Santos <aldoacesso@gmail.com>
2023-11-07 18:51:48 -0300

    Exercício TypeScript

---
+++ src/exercicio_ts.js
@@ -0,0 +1,7 @@
+"use strict";
+const mutiplicacao = (x, y) => x * y;
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(mutiplicacao(4, 2));
+console.log(saudacao("Aldo"));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,9 @@
+const mutiplicacao = (x: number, y: number): number => x * y;
+
+function saudacao(nome: string) {
+    return "Olá " + nome;
+}
+
+console.log(mutiplicacao(4, 2));
+
+console.log(saudacao("Aldo"));
